39df278ff9b9a0f62639e13a46f46159f2316b00
nomeDeus <a19311504@gmail.com>
2017-07-11 14:52:31 +0800

    for checking spoon result and copy it

---
+++ check.py
@@ -0,0 +1,25 @@
+import os.path
+import shutil
+
+from pathlib import Path
+from time import localtime, strftime
+
+#d = datetime.datetime.now()
+
+src = Path("../spoon-output")
+dst = Path("../Results/" + strftime('%Y-%m-%d_%H_%M_%S/', localtime()))
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    print("Copying...")
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+    print("Complete.")
+
+if src.exists():
+    print("File found.")
+    copytree(src, dst)
